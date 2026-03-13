40fbb77f988a0ee5941a9e30919080b26f679bef
Vijayvijo <vijaycj003@gmail.com>
2026-03-13 13:09:42 +0530

    route stucture

--- main.js
+++ main.js
@@ -8,6 +8,29 @@ app.get('/',(req,res) => {
 
 })
 
+//crud functionalities of movies 
+//R for reading
+app.get('/movies', () =>{
+
+})
+
+// C - for creating movies
+app.post('/movies', () =>{
+
+})
+
+//U -Update the movies
+
+app.put('/movies/:id', () =>{
+
+})
+
+//D - Delete the movies
+
+app.delete('/movies/:id', ()=> {
+
+
+})
 app.listen(PORT, ()=>{
     console.log(`The server is running at http://localhost:${6969}`)
 })
