96fa745ea6c1aa800a53a9fd0db6262de7156272
Vijayvijo <vijaycj003@gmail.com>
2026-03-14 13:18:48 +0530

    Understanding middlewares

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -4,7 +4,9 @@ export  const MovieIndex = (req, res) => {
 }
 
 export const MovieCreate = (req, res) => { 
-    res.send("Create a movies lists")
+    // id title desc
+    console.log(req.body)
+    return res.json(req.body)
 }
 
 export const MovieUpdate =  (req, res) => {
--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-import express from "express"
+import express, { urlencoded } from "express"
 import movieRoutes from "./routes/main.routes.js"
 import connectDb from "./lib/db.js"
 
 
+
+
+const app = express()
+const PORT = 6969
+
 //connect DB
 
 connectDb()
-const app = express()
-const PORT = 6969
+
+// data understanding middlewares
+app.use(express.json())
+//url encoded
+app.use(express.urlencoded({extended : true}));
 
 app.get('/',(req,res) => {
     res.json({msg : "Hello this is vijay!"})
